03d815b7e176272469a7f4aa433679da9e59e9da
Vikalp Sharma <vikalpsh1234@gmail.com>
2024-08-30 00:09:37 +0530

    fixed minor error

--- components/careers/internships/process/process.js
+++ components/careers/internships/process/process.js
@@ -4,7 +4,7 @@ import stepsData from "../InternSteps/(steps-Data)/steps";
 
 const Process = () => {
   return (
-    <div className="flex flex-col items-center font-montserrat px-[10rem] pb-24">
+    <div className="flex flex-col items-center font-montserrat px-[10rem] pb-24 ">
       <div className="pb-[4rem]">
         <div className="text-4xl my-4 text-center font-bold">
           Process of Selection
@@ -14,7 +14,7 @@ const Process = () => {
           these steps to start your journey with YuvaCracy.
         </div>
       </div>
-      <div className="flex w-full justify-around">
+      <div className="flex w-full justify-center gap-12">
         {stepsData.map((data, index) => (
           <InternSteps
             key={index}
