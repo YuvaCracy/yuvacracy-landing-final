e54a5eb2f10a97229491ae1ca3fc4b19504a7237
Vikalp Sharma <vikalpsh1234@gmail.com>
2024-08-30 00:41:17 +0530

    Created Finalized version

--- public/assetManager.js
+++ public/assetManager.js
@@ -1,5 +1,5 @@
 import HomeHeroImage from "./assets/home/hero/hero-image.png"
-import InternshipImage1 from "./assets/careers/internships/internship-1.png"
+import InternshipImage1 from "./assets/careers/internships/Internship-1.png"
 import InternshipImage2 from "./assets/careers/internships/Internship-2.png"
 import CareersHero1 from "./assets/careers/hero1.png"
 export {
