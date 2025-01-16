893d2efcfdaac04ffe54c117742262f0b5a91c39
Abhay Mishra <abhaym1205@gmail.com>
2025-01-16 10:15:22 +0530

    solved document is not defined and added vikalp changes

--- components/animation/CelebrateAnimation.js
+++ components/animation/CelebrateAnimation.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import Lottie from 'lottie-react';
 import { Celebrate } from '@/public/assetManager';
+import dynamic from 'next/dynamic';
+const DynamicLottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const CelebrateAnimation = () => {
   return (
     <div className='absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-opacity-50'>
       <div className='w-[20rem] h-[20rem] z-10'>
-        <Lottie
+        <DynamicLottie
           animationData={Celebrate}
           autoplay={true}
           loop={false}
